ac8791ff5700c0dc878e6c1e254b8fc009d35d2c
Andrés Martín <monkeyandres@yahoo.com>
2017-02-20 23:38:28 +0100

    Beta 0.7

--- asserts/js/main.js
+++ asserts/js/main.js
@@ -1,11 +1,40 @@
 page('/MyNoteBooks', index);
-page('/MyNoteBooks/mensaje', obtenerCuaderno);
+page('/MyNoteBooks/:lenguaje', obtenerCuaderno);
 page();
 
 function index(){
-	alert('index');
+	$('#contenedor').load('includes/main.html');
 }
 
-function obtenerCuaderno(){
-	$('#contenedor').load('includes/mensaje.html');
+function obtenerCuaderno(ctx){
+	var file = file || "docs/"+ctx.params.lenguaje+".md";
+	var reader = new stmd.DocParser();
+	var writer = new stmd.HtmlRenderer();
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if(xhr.readyState === 4 && xhr.status === 200) {
+		  display(xhr);
+		}
+	};
+
+	function display(xhr) {
+		var parsed = reader.parse(xhr.responseText);
+		var content = writer.renderBlock(parsed);
+		document.querySelector('#contenedor').innerHTML = content;
+
+		/* try to extract h1 title and use as title for page
+		   if no h1, use name of file 
+		*/
+		try {
+		  document.title = document.querySelector('h1').textContent
+		} catch (e) {
+		  document.title = file;
+		}
+	}
+
+	xhr.open('GET', file);
+	xhr.send();
 }
+
+obtenerCuaderno();
+
