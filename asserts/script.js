50ba7d7544a6844566227a1b9f3961066685c80e
monkeyandres <monkeyandres@yahoo.com>
2017-03-18 12:02:12 +0100

    New Version

---
+++ asserts/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//Acordeon de categorias
+let accordion = document.querySelectorAll("aside>p");
+
+for (var i = 0; i < accordion.length; i++) {
+  accordion[i].onclick = function(){
+    let elem = this.nextElementSibling;
+    if (elem.style.display == "block") {
+      elem.style.display = "none";
+    } else {
+      elem.style.display = "block";
+    }
+  }
+}
+
+//Animacion del sidebar
+let menu = document.querySelector(".mdi-menu");
+
+menu.onclick = function(){
+  if(window.innerWidth <= 865){
+    let elem = document.querySelector("aside");
+    let left = window.getComputedStyle(elem).left;
+
+    console.log(left)
+
+    if (left == "-300px"){elem.style.left = 0}
+    else {elem.style.left = "-300px"}
+  }
+}
---
+++ index.md
@@ -0,0 +1,10 @@
+---
+# You don't need to edit this file, it's empty on purpose.
+# Edit theme's home layout instead if you wanna make some changes
+# See: https://jekyllrb.com/docs/themes/#overriding-theme-defaults
+layout: index
+permalink: /
+---
+Hello, this is my personal place where i write my notebooks. You can check it in the links on the side bar.
+Please follow me on my twitter: [@MonkeyDAndres](https://twitter.com/MonkeyDAndres) and on my github account: [@MonkeyAndres](https://github.com/MonkeyAndres).
+I hope you enjoy my notebooks :D
